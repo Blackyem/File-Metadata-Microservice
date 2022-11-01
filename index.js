69d6fce503157f279d38a895d66b122940b51c0b
yelpcamp-profile <kolayem44@gmail.com>
2022-11-01 01:53:23 +0000

    update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
+const dotenv = require("dotenv");
+const cloudinary = require("cloudinary");
+const multer = require('multer');
+const colors = require("colors");
 var cors = require('cors');
-require('dotenv').config()
+
+
+
+
 
 var app = express();
+
+dotenv.config();
+
+const upload = multer({
+  dest: 'uploads/'
+});
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +25,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size,
+    "destination": req.file.destination
+  })
 });
+
+
+const PORT = process.env.PORT;
+
+const listener = app.listen(process.env.PORT || 6010, () => {
+  console.log(`Your app is listening on port ${PORT}`.bold.blue)
+})
